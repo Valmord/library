de6b589c54c91b7b0a5234538edc6b6e4ac91bec
TC <nothing@nothing.com>
2024-12-09 22:43:18 +1300

    Add basic styling and table rendering

--- code.js
+++ code.js
@@ -15,3 +15,17 @@ function addBookToLibrary(name, author, pageCount, haveRead) {
 
 addBookToLibrary('The Hunger Games', 'Suzzane, Collins', 400, false);
 addBookToLibrary('The Hunger Games - Catching Fire', 'Suzzane, Collins', 391, false); 
+
+
+function displayBooks(){
+  const table = document.querySelector('table');
+  myLibrary.forEach(book => {
+    const bookContainer = document.createElement('tr');
+    for (prop in book) {
+      const bookElement = document.createElement('td');
+      bookElement.textContent = book[prop];
+      bookContainer.appendChild(bookElement);
+    }
+    table.appendChild(bookContainer);
+  })
+}
